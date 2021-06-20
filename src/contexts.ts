85f1ed00b1633dd2af6559646d1087fa866974d1
mie998 <nishiwaki.kyoto@gmail.com>
2021-06-20 10:58:54 +0900

    added some configuration files

--- src/contexts.ts
+++ src/contexts.ts
@@ -2,6 +2,7 @@
 import firebase from 'firebase/app';
 
 import { createContext } from 'react';
+import { User } from './services/gamer-shelf/models/user';
 import { gamerShelfTheme } from './theme';
 
 type FirebaseContextValue = {
@@ -12,6 +13,18 @@ type FirebaseContextValue = {
 export const FirebaseContext = createContext<FirebaseContextValue>({
   auth: null,
   db: null,
+});
+
+type UserContextValue = {
+  user: User | null;
+  credential: firebase.auth.UserCredential | null;
+  setCredential: (credential: firebase.auth.UserCredential | null) => void;
+};
+
+export const UserContext = createContext<UserContextValue>({
+  user: null,
+  credential: null,
+  setCredential: () => undefined,
 });
 
 export const gamerShelfThemeContext = createContext(gamerShelfTheme);
---
+++ src/services/gamer-shelf/models/user.ts
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+
+export type User = {
+  id?: string;
+  screenName: string;
+  displayName: string | null;
+  description: string | null;
+  photoUrl: string | null;
+  provider: string;
+  providerUid: string;
+  createdAt: firebase.firestore.Timestamp | null;
+  updatedAt: firebase.firestore.Timestamp | null;
+};
+
+export const blankUser: User = {
+  screenName: '',
+  displayName: null,
+  description: null,
+  photoUrl: null,
+  provider: 'twitter',
+  providerUid: '',
+  createdAt: null,
+  updatedAt: null,
+};
